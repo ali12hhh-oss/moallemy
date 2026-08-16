3293441bf6b8bc9a3b47f0748eb943da6d5a81ca
ali12hhh-oss <ali12.hhh@gmail.com>
2026-08-16 08:34:37 +0300

    Delete test directory

--- test/widget_test.dart
+++
@@ -1,2 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';import 'package:daleel_child/main.dart';
-void main(){testWidgets('Daleel Child starts', (tester) async {await tester.pumpWidget(const DaleelChildApp());expect(find.text('دليل الطفل'),findsWidgets);});}
