0d976d72344debc7611413c896df773b983f497b
ali12hhh-oss <ali12.hhh@gmail.com>
2026-08-21 09:01:31 +0300

    fix: restore standard Flutter release APK output

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,10 +15,6 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-
-        ndk {
-            abiFilters += listOf("armeabi-v7a", "arm64-v8a", "x86_64")
-        }
     }
 
     compileOptions {
